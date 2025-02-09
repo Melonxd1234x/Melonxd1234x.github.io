bdcadb9075488639b81d3e5c8d81635b899afb59
Melonxd1234x <wwinxprw@gmail.com>
2025-02-09 16:04:53 -0400

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,8 @@
+// index.js
+function redireccionar() {
+    setTimeout(function() {
+        window.location.href = "https://a9fm.github.io/lightshot"; // URL de destino
+    }, 3000); // 3000 milisegundos = 3 segundos
+}
+
+window.onload = redireccionar;
